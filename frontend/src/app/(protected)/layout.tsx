e7a422a5c12fd070d622f0aec50ec1818e2ee06d
Brandon G Jonathan <ribellye@gmail.com>
2025-09-17 14:29:47 -0700

    lint-fix

--- frontend/src/app/(protected)/layout.tsx
+++ frontend/src/app/(protected)/layout.tsx
@@ -2,7 +2,7 @@
 "use client";
 
 import React, { useEffect, useState } from "react";
-import { useRouter } from "next/navigation";
+// import { useRouter } from "next/navigation";
 import { useAuth } from "../../context/AuthContext";
 
 export default function ProtectedLayout({
@@ -11,7 +11,7 @@ export default function ProtectedLayout({
   children: React.ReactNode;
 }) {
   const { user, loading } = useAuth(); // throws if outside provider
-  const router = useRouter();
+  // const router = useRouter();
   const [hydrated, setHydrated] = useState(false);
 
   // Avoid hydration mismatch
